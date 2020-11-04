f7aa3ddac627df4fde3f4e7ef2c62fd8425fe564
Christian Polonio <christian.polonio@duerrdental.com>
2020-11-04 23:02:59 +0100

    added arithmetic functions

--- Class1.cs
+++ Class1.cs
@@ -4,5 +4,15 @@ namespace GitPlayground
 {
     public class Class1
     {
+       public int Add(int a, int b)
+       {
+          return a + b;
+       }
+
+       public int Sub(int a, int b)
+       {
+          return a - b;
+       }
+
     }
 }
